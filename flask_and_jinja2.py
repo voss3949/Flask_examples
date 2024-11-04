c16223fe6f0afc8e958bd822a52f215722bf4647
voss3949 <54409097+voss3949@users.noreply.github.com>
2024-11-03 20:34:44 -0500

    Update flask_and_jinja2.py

--- flask_and_jinja2.py
+++ flask_and_jinja2.py
@@ -1,9 +1,15 @@
+import os
 from flask import Flask, render_template
-app = Flask(__name__)
+
+template_dir = os.path.abspath('C:/Users/12245/OneDrive/Documents/Python_Projects/MyDashboard/')
+
+app = Flask(__name__, template_folder=template_dir)
+Name = 'Joshua'
 
 @app.route("/")
-def hello():
-    return render_template('index.html', name='World')
+def homepage():
+    return render_template("index.html", name=Name)
+
 
 if __name__ == "__main__":
-	app.run()
+	app.run(port=5000)
