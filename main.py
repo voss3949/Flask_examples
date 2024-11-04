4680a30dbfbfabf08adb0fd4b6f877e46ba7bc43
voss3949 <54409097+voss3949@users.noreply.github.com>
2024-11-03 20:39:50 -0500

    Update and rename flask_and_jinja2.py to main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import os
 from flask import Flask, render_template
+import yfinance as yf
 
 template_dir = os.path.abspath('C:/Users/12245/OneDrive/Documents/Python_Projects/MyDashboard/')
 
 app = Flask(__name__, template_folder=template_dir)
-Name = 'Joshua'
+
+name = 'Joshua'
+btc_price = yf.Ticker("BTC-USD").history(period='1d')['Close'].iloc[-1]
 
 @app.route("/")
 def homepage():
-    return render_template("index.html", name=Name)
+    return render_template("index.html", name=name, btc_price=btc_price)
 
 
 if __name__ == "__main__":
-	app.run(port=5000)
+    app.run(port=5000)
