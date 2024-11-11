3510f3e2e66918fe2ff25ba8d8e1e0a7b594e337
voss3949 <54409097+voss3949@users.noreply.github.com>
2024-11-11 01:13:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template
 import yfinance as yf
 
+#C:/Users/U2 Security/Documents/
 template_dir = os.path.abspath('C:/Users/12245/OneDrive/Documents/Python_Projects/MyDashboard/')
 
 app = Flask(__name__, template_folder=template_dir)
@@ -13,6 +14,6 @@ btc_price = yf.Ticker("BTC-USD").history(period='1d')['Close'].iloc[-1]
 def homepage():
     return render_template("index.html", name=name, btc_price=btc_price)
 
-
+#127.0.0.1:5000
 if __name__ == "__main__":
     app.run(port=5000)
